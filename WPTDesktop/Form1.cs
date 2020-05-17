0ed8025f8531a8136009a24ba81f1235fd41d8eb
IgorZmikic <65419153+IgorZmikic@users.noreply.github.com>
2020-05-18 00:38:45 +0200

    Dodan package Microsoft.AspNet.WebApi.Client za json, dodan DataTable i dataGridView, test serijelizacije

--- WPTDesktop/Form1.cs
+++ WPTDesktop/Form1.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace WPTDesktop
 {
     public partial class Form1 : Form
     {
+        WPTService.WebService proxy = new WPTService.WebService();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int organisationID = 1; //samo za test
+            string printersList = proxy.GetPrinters(organisationID);
+            DataTable dtPrinters = new DataTable();
+
+            //Dodan package Microsoft.AspNet.WebApi.Client za json
+
+            dtPrinters = JsonConvert.DeserializeObject<DataTable>(printersList);
+
+            dataGridView1.DataSource = dtPrinters;
         }
     }
 }
